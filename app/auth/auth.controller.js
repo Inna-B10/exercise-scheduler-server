b8407bf7c07b1ff27bc61a7c779a44f008ba415f
inna.b10 <inna.b10.kodehode@gmail.com>
2024-09-24 09:37:34 +0200

    install prisma, create connection to DB

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,7 +1,10 @@
+import { prisma } from '../prisma.js'
+
 //@desc     Auth user
 //@route    POST /api/auth/login
 //@access   Public
 
 export const authUser = async (req, res) => {
-	res.json({ message: 'You are authenticated' })
+	const user = await prisma.user.findMany()
+	res.json(user)
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import express from 'express'
 import morgan from 'morgan'
 import authRoutes from './app/auth/auth.routes.js'
+import { prisma } from './app/prisma.js'
 
 dotenv.config()
 
@@ -23,3 +24,11 @@ async function main() {
 	)
 }
 main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async e => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
