43cd14b02b97a4bfa31abbad5850ff064b8b7a88
inna.b10 <inna.b10.kodehode@gmail.com>
2024-09-24 15:10:52 +0200

    added route /register, function registerUser

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,5 +1,8 @@
+import { hash } from 'argon2'
 import asyncHandler from 'express-async-handler'
 import { prisma } from '../prisma.js'
+import { UserFields } from '../utils/user.utils.js'
+import { generateToken } from './generate-token.js'
 
 //@desc     Auth user
 //@route    POST /api/auth/login
@@ -12,4 +15,33 @@ export const authUser = asyncHandler(async (req, res) => {
 		}
 	})
 	res.json(user)
+})
+
+//@desc     Register user
+//@route    POST /api/auth/register
+//@access   Public
+export const registerUser = asyncHandler(async (req, res) => {
+	const { email, password, nickname } = req.body
+	const isHaveUser = await prisma.user.findUnique({
+		where: {
+			email: email
+		}
+	})
+
+	if (isHaveUser) {
+		res.status(400)
+		throw new Error('User already exists ')
+	}
+
+	const user = await prisma.user.create({
+		data: {
+			email,
+			password: await hash(password),
+			nickname
+		},
+		select: UserFields
+	})
+
+	const token = generateToken(user.id)
+	res.json(user, token)
 })
