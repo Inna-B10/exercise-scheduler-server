a46cb802feba7c8aaa44bcc79c5566b8f907bcdb
inna.b10 <inna.b10.kodehode@gmail.com>
2024-09-25 15:24:51 +0200

    realized update and delete exercise

--- app/exercise/exercise.controller.js
+++ app/exercise/exercise.controller.js
@@ -23,7 +23,53 @@ export const createNewExercise = asyncHandler(async (req, res) => {
 //@access	Private
 
 export const getExercises = asyncHandler(async (req, res) => {
-	const exercises = await prisma.exercise.findMany()
+	const exercises = await prisma.exercise.findMany({
+		orderBy: {
+			createdAt: 'desc'
+		}
+	})
 
 	res.json(exercises)
+})
+//@desc 	Update exercise
+//@route	PUT /api/exercises/:id
+//@access	Private
+
+export const updateExercise = asyncHandler(async (req, res) => {
+	const { name, times, iconPath } = req.body
+	console.log(req.body)
+	try {
+		const exercise = await prisma.exercise.update({
+			where: {
+				id: Number(req.params.id) //+req.params.id
+			},
+			data: {
+				name,
+				times,
+				iconPath
+			}
+		})
+		res.json(exercise)
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found')
+	}
+})
+//@desc 	Delete exercise
+//@route	Delete /api/exercises/:id
+//@access	Private
+
+export const deleteExercise = asyncHandler(async (req, res) => {
+	try {
+		const exercise = await prisma.exercise.delete({
+			where: {
+				id: +req.params.id //Number(req.params.id)
+			}
+		})
+
+		res.json({ message: 'Exercise deleted!' })
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found')
+	}
 })
--- app/exercise/exercise.routes.js
+++ app/exercise/exercise.routes.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import { protect } from '../middleware/auth.middleware.js'
-import { createNewExercise, getExercises } from './exercise.controller.js'
+import {
+	createNewExercise,
+	deleteExercise,
+	getExercises,
+	updateExercise
+} from './exercise.controller.js'
 
 const router = express.Router()
 
 router.route('/').post(protect, createNewExercise)
 router.route('/').get(protect, getExercises)
+router.route('/:id').put(protect, updateExercise)
+router.route('/:id').delete(protect, deleteExercise)
 
 export default router
