40f424663420bf2e94019ee33488ee9a69eee7ad
inna.b10 <inna.b10.kodehode@gmail.com>
2024-09-26 23:03:37 +0200

    added ExerciseLog and WorkoutLog models

---
+++ prisma/migrations/20240926102535_created_exercise_log_and_workout_log_models/migration.sql
@@ -0,0 +1,57 @@
+-- AlterTable
+ALTER TABLE "Exercise" ADD COLUMN     "exercise_log_id" INTEGER;
+
+-- CreateTable
+CREATE TABLE "Exercise_time" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "weight" INTEGER NOT NULL,
+    "repeat" INTEGER NOT NULL,
+    "is_completed" BOOLEAN NOT NULL DEFAULT false,
+    "exercise_log_id" INTEGER,
+
+    CONSTRAINT "Exercise_time_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Exercise_log" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "is_completed" BOOLEAN NOT NULL DEFAULT false,
+    "user_id" INTEGER,
+    "workout_log_id" INTEGER,
+
+    CONSTRAINT "Exercise_log_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Workout_log" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "is_completed" BOOLEAN NOT NULL DEFAULT false,
+    "workout_id" INTEGER,
+    "user_id" INTEGER,
+
+    CONSTRAINT "Workout_log_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Exercise" ADD CONSTRAINT "Exercise_exercise_log_id_fkey" FOREIGN KEY ("exercise_log_id") REFERENCES "Exercise_log"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Exercise_time" ADD CONSTRAINT "Exercise_time_exercise_log_id_fkey" FOREIGN KEY ("exercise_log_id") REFERENCES "Exercise_log"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Exercise_log" ADD CONSTRAINT "Exercise_log_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Exercise_log" ADD CONSTRAINT "Exercise_log_workout_log_id_fkey" FOREIGN KEY ("workout_log_id") REFERENCES "Workout_log"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Workout_log" ADD CONSTRAINT "Workout_log_workout_id_fkey" FOREIGN KEY ("workout_id") REFERENCES "Workout"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Workout_log" ADD CONSTRAINT "Workout_log_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ prisma/migrations/20240926104732_added_default_values_for_exercise_time/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Exercise_time" ALTER COLUMN "weight" SET DEFAULT 0,
+ALTER COLUMN "repeat" SET DEFAULT 0;
